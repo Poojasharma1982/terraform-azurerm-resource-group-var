72d8aaca18dce7f9d7b4bd35d5bd954405f62df8
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-28 20:41:08 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,9 @@
-variable "rg-name" {
+variable "rg-group-name" {
   type = string
   #default = "01000000-0000-4000-8000-000030080200"
+  
+}
+variable "rg-group-location" {
+  type = string
+  #default = "01000000-0000-4000-8000-000030080200" 
 }
